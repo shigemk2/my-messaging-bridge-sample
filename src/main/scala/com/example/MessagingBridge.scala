6e047bfb8912943d7598c0c98bce39801cda4db2
shigemk2 <i.am.shige@gmail.com>
2016-11-11 01:07:18 +0900

    Add MessageBridgeException

--- src/main/scala/com/example/MessagingBridge.scala
+++ src/main/scala/com/example/MessagingBridge.scala
@@ -2,5 +2,11 @@ package com.example
 
 import akka.actor._
 
-object MessagingBridgeDriver {
+class MessageBridgeException(message: String, cause: Throwable, retry: Boolean)
+  extends RuntimeException(message, cause) {
+
+  def this(message: String, cause: Throwable) =
+    this(message, cause, false)
+
+  def retry(): Boolean = retry
 }
