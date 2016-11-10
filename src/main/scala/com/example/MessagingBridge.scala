201275f8e19abbd40d160dbbc38840aa8e673106
shigemk2 <i.am.shige@gmail.com>
2016-11-11 01:10:39 +0900

    Add InventoryProductAllocationBridge

--- src/main/scala/com/example/MessagingBridge.scala
+++ src/main/scala/com/example/MessagingBridge.scala
@@ -1,6 +1,7 @@
 package com.example
 
 import akka.actor._
+import akka.event.{Logging, LoggingAdapter}
 
 class MessageBridgeException(message: String, cause: Throwable, retry: Boolean)
   extends RuntimeException(message, cause) {
@@ -12,3 +13,17 @@ class MessageBridgeException(message: String, cause: Throwable, retry: Boolean)
 }
 
 object RabbitMQMessagingBridgeRunner extends CompletableApp(1)
+
+class InventoryProductAllocationBridge(config: RabbitMQBridgeConfig)
+  extends RabbitMQBridgeActor(config) {
+  private val log: LoggingAdapter = Logging.getLogger(context.system, self)
+
+  def receive = {
+    case message: RabbitMQBinaryMessage =>
+      log.error("Binary messages not supported.")
+    case message: RabbitMQTextMessage =>
+      log.error(s"Received text message: ${message.textMessage}")
+    case invalid: Any =>
+      log.error(s"Don't understand message: $invalid")
+  }
+}
